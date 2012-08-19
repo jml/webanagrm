b7b005620a68a391668f3731d2182c9b233dcdee
Jonathan Lange <jml@mumak.net>
2012-08-20 00:43:23 +0100

    Highly manual bootstrap starter template page.

--- src/webanagrm/views/common.clj
+++ src/webanagrm/views/common.clj
@@ -4,6 +4,29 @@
   (:require [hiccup.bootstrap.page :refer [include-bootstrap fixed-layout]]))
 
 
+(defpartial navbar []
+  [:div.navbar.navbar-fixed-top
+   [:div.navbar-inner
+    [:div.container
+     [:a.btn.btn-navbar {:data-toggle "collapse" :data-target ".nav-collapse"}
+      [:span.icon-bar]
+      [:span.icon-bar]
+      [:span.icon-bar]]
+     [:a.brand {:href "#"} "Project name"]
+     [:div.nav-collapse
+      [:ul.nav
+       [:li.active [:a {:href "#"} "Home"]]
+       [:li [:a {:href "#about"} "About"]]
+       [:li [:a {:href "#contact"} "Contact"]]]]
+     "<!--/.nav-collapse -->"
+     ]]])
+
+
+(defonce ie-html5-shim
+  (str "<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->"
+       "<!--[if lt IE 9]>"
+       "<script src=\"http://html5shim.googlecode.com/svn/trunk/html5.js\"></script>"
+       " <![endif]-->"))
 
 (defpartial layout [& content]
   (html5
@@ -13,6 +36,10 @@
     [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
     [:meta {:name "description" :content ""}]
     [:meta {:name "author" :content ""}]
-    (include-bootstrap)]
+    (include-bootstrap)
+    ie-html5-shim
+    [:style "body { padding-top: 60px; padding-bottom: 40px;}"]
+    ]
    [:body
+    (navbar)
     (fixed-layout content)]))

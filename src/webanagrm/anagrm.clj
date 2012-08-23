6ac46c021a726c1956253f5b4e598c3f15ef7313
Jonathan Lange <jml@mumak.net>
2012-08-23 22:38:57 +0100

    Add the anagrm logic and include a view that retrieves it.

---
+++ src/webanagrm/anagrm.clj
@@ -0,0 +1,30 @@
+(ns webanagrm.anagrm
+  (:require [clojure.java.io :refer [reader]]
+            [clojure.string :refer [lower-case]]))
+
+(defonce ^:const dictionary-file "/usr/share/dict/words")
+
+(defn anagram?
+  "Is x an anagram of y?"
+  [x y]
+  (= (sort x) (sort y)))
+
+(defn sub-frequency? [a b]
+  (every? #(>= (get a (key %) -1) (val %)) b))
+
+(defn sub-anagram? [base-word sub-word]
+  (sub-frequency? (frequencies (lower-case base-word)) (frequencies (lower-case sub-word))))
+
+(defn find-anagrams [base-word word-list]
+  (filter (partial anagram? base-word) word-list))
+
+(defn word-search [search?]
+  (with-open [dict (reader dictionary-file)]
+    (filter search? (line-seq dict))))
+
+(defn smh-target-puzzle
+  [letters compulsory-letter min-length]
+  #(and (>= (count %)
+            min-length)
+        (contains? (set %) compulsory-letter)
+        (sub-anagram? letters %)))
--- src/webanagrm/views/welcome.clj
+++ src/webanagrm/views/welcome.clj
@@ -1,6 +1,9 @@
 (ns webanagrm.views.welcome
-  (:require [webanagrm.views.common :as common])
+  (:require [webanagrm.views.common :as common]
+            [webanagrm.anagrm :refer [smh-target-puzzle dictionary-file]]
+            [clojure.java.io :refer [reader]])
   (:use [noir.core :only [defpage]]
+        [noir.response :only [json]]
         [hiccup.page :only [include-css html5]]))
 
 
@@ -8,3 +11,8 @@
   (common/layout
    [:h1 "webanagrm"]
    [:p "This will be a thing that solves Target puzzles"]))
+
+
+(defpage [:get "/search"] {:keys [word letter]}
+  (with-open [dict (reader dictionary-file)]
+    (doall (json (filter (smh-target-puzzle word (first letter) 4) (line-seq dict))))))
